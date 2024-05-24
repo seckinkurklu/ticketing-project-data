79bbeaae4b01559da9da5568f6503929e229c531
seckinkurklu <kurkluseckin@gmail.com>
2024-05-24 12:53:58 +0300

    Ticketing Data

---
+++ src/main/java/com/cydeo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserDTO> listAllUsers();
+    UserDTO findByUserName(String username);
+    void save(UserDTO user);
+    void deleteByUserName();
+
+}
