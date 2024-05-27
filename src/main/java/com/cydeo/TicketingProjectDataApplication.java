2d0e14e9d9f17ef0b095b287564a686ca4c1160e
seckinkurklu <kurkluseckin@gmail.com>
2024-05-27 23:20:48 +0300

    Ticketing Data

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -12,7 +12,6 @@ public class TicketingProjectDataApplication {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
 
-    ModelMapper mapper = new ModelMapper();
 
     @Bean
     public ModelMapper mapper(){
