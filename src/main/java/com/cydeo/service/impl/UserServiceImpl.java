f5201caa8406c7f0405d3a47f792a5c29d28ae10
seckinkurklu <kurkluseckin@gmail.com>
2024-05-31 09:49:50 +0300

    Ticketing Data

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -2,20 +2,24 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
+import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -23,9 +27,7 @@ public class UserServiceImpl implements UserService {
 
      List<User> userList = userRepository.findAll(Sort.by("firstName"));
 
-
-
-        return null;
+     return userList.stream().map(userMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
