b6c7c64272dfb22be31f2c2d472a360217b652ad
seckinkurklu <kurkluseckin@gmail.com>
2024-06-06 11:23:34 +0300

    Ticketing Data

--- src/main/java/com/cydeo/converter/ProjectDtoConverter.java
+++ src/main/java/com/cydeo/converter/ProjectDtoConverter.java
@@ -2,6 +2,7 @@ package com.cydeo.converter;
 
 import com.cydeo.dto.ProjectDTO;
 //import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import com.cydeo.service.ProjectService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -9,27 +10,22 @@ import org.springframework.stereotype.Component;
 //@ConfigurationPropertiesBinding
 public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
 
-     @Override
-    public ProjectDTO convert(String source) {
-        return null;
+
+    ProjectService projectService;
+
+    public ProjectDtoConverter(ProjectService projectService) {
+        this.projectService = projectService;
     }
 
+    @Override
+    public ProjectDTO convert(String source) {
 
-//    ProjectService projectService;
-//
-//    public ProjectDtoConverter(ProjectService projectService) {
-//        this.projectService = projectService;
-//    }
-//
-//    @Override
-//    public ProjectDTO convert(String source) {
-//
-//        if (source == null || source.equals("")) {
-//            return null;
-//        }
-//
-//        return projectService.findById(source);
-//
-//    }
+        if (source == null || source.equals("")) {
+            return null;
+        }
+
+        return projectService.getByProjectCode(source);
+
+    }
 
 }
