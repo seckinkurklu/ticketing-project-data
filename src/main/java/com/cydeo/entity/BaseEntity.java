68f473fcaa0b2b694285d167c2a533da6e62f2df
seckinkurklu <kurkluseckin@gmail.com>
2024-06-06 11:04:57 +0300

    Ticketing Data

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -2,10 +2,7 @@ package com.cydeo.entity;
 
 import lombok.*;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -20,11 +17,17 @@ public class BaseEntity {
     private Long id;
 
     private Boolean isDeleted = false;
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
 
+    @PrePersist
     private void onPrePersist(){
         this.insertDateTime = LocalDateTime.now();
         this.lastUpdateDateTime = LocalDateTime.now();
@@ -32,6 +35,7 @@ public class BaseEntity {
         this.lastUpdateUserId = 1L;
     }
 
+    @PreUpdate
     private void onPreUpdate(){
         this.lastUpdateDateTime=LocalDateTime.now();
         this.lastUpdateUserId=1L;
